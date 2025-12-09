b9878a3a8d649072cfe9ef083f3a2a7aa6646fac
christiantondo <christiantondo1@gmail.com>
2025-12-10 00:41:23 +0100

    icons hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,27 +5,27 @@ function Footer() {
 				<div className="container-boxed icons-spacing">
 					<div className="flex-item">
 						<img className="icons-style" src="/buy-comics-digital-comics.png" alt="" />
-						<p className="icon-text">DIGITAL COMICS</p>
+						<p className="icons-text">DIGITAL COMICS</p>
 					</div>
 
 					<div className="flex-item">
 						<img className="icons-style" src="/buy-comics-merchandise.png" alt="" />
-						<p className="icon-text">DC MERCHANDISE</p>
+						<p className="icons-text">DC MERCHANDISE</p>
 					</div>
 
 					<div className="flex-item">
 						<img className="icons-style" src="/buy-comics-shop-locator.png" alt="" />
-						<p className="icon-text">SUBSCRIPTION</p>
+						<p className="icons-text">SUBSCRIPTION</p>
 					</div>
 
 					<div className="flex-item">
 						<img className="icons-style" src="/buy-comics-subscriptions.png" alt="" />
-						<p className="icon-text">COMIC SHOP LOCATOR</p>
+						<p className="icons-text">COMIC SHOP LOCATOR</p>
 					</div>
 
 					<div className="flex-item">
 						<img className="icons-style" src="/buy-comics-subscriptions.png" alt="" />
-						<p className="icon-text">DC POWER VISA</p>
+						<p className="icons-text">DC POWER VISA</p>
 					</div>
 
 				</div>
