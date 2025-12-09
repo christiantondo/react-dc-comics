bfa35a25b93f3b0c32afbe67733b19a7bd534944
christiantondo <christiantondo1@gmail.com>
2025-12-09 23:17:39 +0100

    raw footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,35 @@
 function Footer() {
-    return (
-        <footer>Footer</footer>
-    )
+	return (
+		<footer>
+			<div className="footer-icons-section">
+
+			</div>
+
+			<div className="footer-list-section">
+				<div className="container-boxed flex-container">
+					<div>
+						<ul>
+							<li>CHARACTERS</li>
+							<li>COMICS</li>
+							<li>MOVIES</li>
+							<li>TV</li>
+							<li>GAMES</li>
+							<li>COLLECTIBLES</li>
+							<li>VIDEOS</li>
+							<li>FANS</li>
+							<li>NEWS</li>
+							<li>SHOP</li>
+						</ul>
+					</div>
+					<img src="/dc-logo-bg.png" alt="" id="dc-logo-list-bg" />
+				</div>
+			</div>
+
+			<div className="footer-social-section">
+
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
