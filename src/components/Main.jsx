bf0497b723b2e015d3dd056ec25acf10321ab19e
christiantondo <christiantondo1@gmail.com>
2025-12-09 22:55:53 +0100

    raw main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
 function Main() {
-    return (
-        <main>Main</main>
-    )
+	return (
+		<main>
+			<div className="container-boxed">
+				<h2>--&gt; Content goes here &lt;--</h2>
+			</div>
+		</main>
+	)
 }
 
 export default Main
