c205d58ad0d6b3228c118885f8fe743d9b0eeb58
christiantondo <christiantondo1@gmail.com>
2025-12-12 17:40:50 +0100

    social icon section code ordered

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,11 +34,27 @@ function Footer() {
 					<div className="flex-item align-center">
 
 						<p id="follow-us-style">FOLLOW US</p>
-						<a href="https://www.facebook.com/dc/?brand_redir=248819758864166" target="blank"><img src="/footer-facebook.png" className="social-icons" alt="" /></a>
-						<a href="https://maps.app.goo.gl/Bz7eRmazYGsZM1qA7" target="blank"><img src="/footer-periscope.png" className="social-icons" alt="" /></a>
-						<a href="https://it.pinterest.com/dccomics/" target="blank"><img src="/footer-pinterest.png" className="social-icons" alt="" /></a>
-						<a href="https://x.com/DCOfficial" target="blank"><img src="/footer-twitter.png" className="social-icons" alt="" /></a>
-						<a href="https://www.youtube.com/@dcofficial" target="blank"><img src="/footer-youtube.png" className="social-icons" alt="" /></a>
+
+						<a href="https://www.facebook.com/dc/?brand_redir=248819758864166" target="blank">
+							<img src="/footer-facebook.png" className="social-icons" alt="" />
+						</a>
+
+						<a href="https://maps.app.goo.gl/Bz7eRmazYGsZM1qA7" target="blank">
+							<img src="/footer-periscope.png" className="social-icons" alt="" />
+						</a>
+
+						<a href="https://it.pinterest.com/dccomics/" target="blank">
+							<img src="/footer-pinterest.png" className="social-icons" alt="" />
+						</a>
+
+						<a href="https://x.com/DCOfficial" target="blank">
+							<img src="/footer-twitter.png" className="social-icons" alt="" />
+						</a>
+
+						<a href="https://www.youtube.com/@dcofficial" target="blank">
+							<img src="/footer-youtube.png" className="social-icons" alt="" />
+						</a>
+
 					</div>
 				</div>
 
