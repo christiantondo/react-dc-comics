b1d5718a86b7d7b9ee49a4499b75f11e6ae0cc15
christiantondo <christiantondo1@gmail.com>
2025-12-15 20:26:56 +0100

    footer list completed and header text updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,64 @@ import style from "./Footer.module.css"
 function Footer() {
 	return (
 		<footer>
-			<div className={style.listSection}>
+			<div className={`${style.listSection}`}>
 				<div className="containerBoxed flexContainer alignCenter">
-					<div>
-						<ul>
-							<li>CHARACTERS</li>
-							<li>COMICS</li>
-							<li>MOVIES</li>
-							<li>TV</li>
-							<li>GAMES</li>
-							<li>COLLECTIBLES</li>
-							<li>VIDEOS</li>
-							<li>FANS</li>
-							<li>NEWS</li>
-							<li>SHOP</li>
-						</ul>
+
+					<div className={`flexContainer alignStart ${style.column}`}>
+						<div>
+							<h3 className={style.listTitleFontStyle}>DC COMICS</h3>
+							<ul className={`${style.shopColumn}`}>
+								<li>Characters</li>
+								<li>Comics</li>
+								<li>Movies</li>
+								<li>TV</li>
+								<li>Games</li>
+								<li>Videos</li>
+								<li>News</li>
+							</ul>
+
+							<h3 className={style.listTitleFontStyle}>SHOP</h3>
+							<ul>
+								<li>Shop DC</li>
+								<li>Shop DC Collectibles</li>
+							</ul>
+						</div>
+
+						<div>
+							<h3 className={style.listTitleFontStyle}>DC</h3>
+							<li>Terms Of Use</li>
+							<li>Privacy Policy (New)</li>
+							<li>Ad Choices</li>
+							<li>Advertising</li>
+							<li>Jobs</li>
+							<li>Subscriptions</li>
+							<li>Talent Workshops</li>
+							<li>CPSC Certificates</li>
+							<li>Ratings</li>
+							<li>Shop Help</li>
+							<li>Contact Us</li>
+
+						</div>
+
+						<div>
+							<h3 className={style.listTitleFontStyle}>SITES</h3>
+							<ul>
+								<li>DC</li>
+								<li>MAD Magazine</li>
+								<li>DC Kids</li>
+								<li>DC Universe</li>
+								<li>DC Power Visa</li>
+							</ul>
+						</div>
 					</div>
 
 					<div>
 						<img src="/dc-logo-bg.png" alt="" className={style.dcLogo} />
 					</div>
+
 				</div>
+
+
 			</div>
 
 			<div className={style.socialSection}>
