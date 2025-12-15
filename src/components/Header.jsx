eca428e40f319d417e5b7e527f1cb0718662da18
christiantondo <christiantondo1@gmail.com>
2025-12-15 22:04:27 +0100

    array of object for nav created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,56 @@
 import style from "./Header.module.css"
 
 const links = [
-	"CHARACTERS",
-	"COMICS",
-	"MOVIES",
-	"TV",
-	"GAMES",
-	"COLLECTIBLES",
-	"VIDEOS",
-	"FANS",
-	"NEWS",
-	"SHOP"
+	{
+		id: 1,
+		text: "CHARACTERS",
+		url: "#"
+	},
+	{
+		id: 2,
+		text: "COMICS",
+		url: "#"
+	},
+	{
+		id: 3,
+		text: "MOVIES",
+		url: "#"
+	},
+	{
+		id: 4,
+		text: "TV",
+		url: "#"
+	},
+	{
+		id: 5,
+		text: "GAMES",
+		url: "#"
+	},
+	{
+		id: 6,
+		text: "COLLECTIBLES",
+		url: "#"
+	},
+	{
+		id: 7,
+		text: "VIDEOS",
+		url: "#"
+	},
+	{
+		id: 8,
+		text: "FANS",
+		url: "#"
+	},
+	{
+		id: 9,
+		text: "NEWS",
+		url: "#"
+	},
+	{
+		id: 10,
+		text: "SHOP",
+		url: "#"
+	}
 ]
 
 function Header() {
