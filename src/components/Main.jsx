2e7cdf7623783b0c8c6305c77057f5b68d58f815
christiantondo <christiantondo1@gmail.com>
2025-12-16 13:23:46 +0100

    comics array imported in project

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import style from "./Main.module.css"
 function Main() {
 	return (
 		<main >
-			<div className={style.jumbotron}></div>
+			<div className={style.jumbotron}>\</div>
 
 			<div className={`${style.containerBoxed} ${style.alignCenter} ${style.comicSection}`} >
 
