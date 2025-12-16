885ab602f2e761533e3e6996c67530fe6f1b212a
christiantondo <christiantondo1@gmail.com>
2025-12-16 13:34:40 +0100

    data successfully imported

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import style from "./Main.module.css"
+import comics from "../data/comics"
 
 function Main() {
 	return (
@@ -6,6 +7,11 @@ function Main() {
 			<div className={style.jumbotron}>\</div>
 
 			<div className={`${style.containerBoxed} ${style.alignCenter} ${style.comicSection}`} >
+
+				{comics.map(comic => {
+					return <div>{comic.title}</div>
+				})
+				}
 
 			</div>
 		</main >
