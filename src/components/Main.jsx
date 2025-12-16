1e0486d3f9b80d6cecc7bfb50a2becb12c12b1bf
christiantondo <christiantondo1@gmail.com>
2025-12-16 13:12:25 +0100

    jumbotron added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ function Main() {
 	return (
 		<main className={style.main}>
 			<div className={`${style.containerBoxed} ${style.alignCenter}`} >
-				<h2> --&gt; Content goes here &lt;-- </h2>
+
 			</div>
 		</main >
 	)
