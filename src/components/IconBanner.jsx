4f741c3a57421d0196ec17e3126269bc5defca28
christiantondo <christiantondo1@gmail.com>
2026-01-07 19:33:57 +0100

    icons are now inserted in blu banner through map method

--- src/components/IconBanner.jsx
+++ src/components/IconBanner.jsx
@@ -7,41 +7,12 @@ function IconBanner() {
 
             <div className={`containerBoxed ${style.iconSpacing}`} >
 
-                <div className="flexItem">
-
-                    <img className={style.iconStyle} src="/buy-comics-digital-comics.png" alt="" />
-                    <p className={style.iconText}>DIGITAL COMICS</p>
-
-                </div>
-
-
-                {          /*      <div className="flexItem">
-
-                    <img className={style.iconStyle} src="/buy-comics-merchandise.png" alt="" />
-                    <p className={style.iconText}>DC MERCHANDISE</p>
-
-                </div>
-
-                <div className="flexItem">
-
-                    <img className={style.iconStyle} src="/buy-comics-shop-locator.png" alt="" />
-                    <p className={style.iconText}>SUBSCRIPTION</p>
-
-                </div>
-
-                <div className="flexItem">
-
-                    <img className={style.iconStyle} src="/buy-comics-subscriptions.png" alt="" />
-                    <p className={style.iconText}>COMIC SHOP LOCATOR</p>
-
-                </div>
-
-                <div className="flexItem">
-
-                    <img className={style.iconStyle} src="/buy-dc-power-visa.svg" alt="" />
-                    <p className={style.iconText}>DC POWER VISA</p>
-
-                </div> */}
+                {bannerIcons.map((icon =>
+                    <div className="flexItem">
+                        <img className={style.iconStyle} src={icon.src} alt="" />
+                        <p className={style.iconText}>{icon.text}</p>
+                    </div>
+                ))}
 
             </div>
 
--- src/data/bannerIcons.js
+++ src/data/bannerIcons.js
@@ -2,27 +2,27 @@
 const bannerIcons = [
     {
         id: 1,
-        img: "/buy-comics-digital-comics.png",
+        src: "/buy-comics-digital-comics.png",
         text: "DIGITAL COMICS"
     },
     {
         id: 2,
-        img: "/buy-comics-merchandise.png",
+        src: "/buy-comics-merchandise.png",
         text: "DC MERCHANDISE"
     },
     {
         id: 3,
-        img: "/buy-comics-shop-locator.png",
+        src: "/buy-comics-shop-locator.png",
         text: "SUBSCRIPTION"
     },
     {
         id: 4,
-        img: "/buy-comics-subscriptions.png",
+        src: "/buy-comics-subscriptions.png",
         text: "COMIC SHOP LOCATOR"
     },
     {
         id: 5,
-        img: "/buy-dc-power-visa.svg",
+        src: "/buy-dc-power-visa.svg",
         text: "DC POWER VISA"
     }
 ]
